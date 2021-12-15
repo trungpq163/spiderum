f424820fa223280bf05844b95e59f263097c1b1e
trungpq163 <miyucoder@gmail.com>
2021-12-15 19:28:58 +0700

    Try to fix LightHouse CI

--- lighthouserc.js
+++ lighthouserc.js
@@ -3,12 +3,17 @@ module.exports = {
     collect: {
       // Static site
       staticDistDir: 'dist/apps/spiderumapp/.next',
-      // Dynamic site
-      startServerCommand: 'npx nx serve spiderumapp',
-      url: ['http://localhost:4200'],
+      budgetPath: './budget.json',
     },
     upload: {
       target: 'temporary-public-storage',
+    },
+    assert: {
+      assertions: {
+        // This setting asserts that the budgets audit passed in Lighthouse CI
+        // under 3 sec on slow 3G connection
+        'performance-budget': 'error',
+      },
     },
   },
 };
