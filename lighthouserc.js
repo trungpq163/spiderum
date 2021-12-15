7d3534740c02ab725384d226ced38081cd9841f4
trungpq163 <miyucoder@gmail.com>
2021-12-15 21:02:58 +0700

    Refactor home page

--- lighthouserc.js
+++ lighthouserc.js
@@ -2,18 +2,18 @@ module.exports = {
   ci: {
     collect: {
       // Static site
-      staticDistDir: './dist/apps/spiderumapp/.next',
-      // budgetPath: './budget.json',
+      staticDistDir: 'dist/apps/spiderumapp/.next',
+      budgetPath: './budget.json',
     },
     upload: {
       target: 'temporary-public-storage',
     },
-    // assert: {
-    //   assertions: {
-    //     // This setting asserts that the budgets audit passed in Lighthouse CI
-    //     // under 3 sec on slow 3G connection
-    //     'performance-budget': 'error',
-    //   },
-    // },
+    assert: {
+      assertions: {
+        // This setting asserts that the budgets audit passed in Lighthouse CI
+        // under 3 sec on slow 3G connection
+        'performance-budget': 'error',
+      },
+    },
   },
 };
